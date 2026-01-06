a46c821d2d9770195d1ed61800215f2bfb540779
Diamond Savior <devbydiamond24.7@gmail.com>
2026-01-05 16:18:24 -0800

    Repair navigation/AppNavigation.js

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -9,6 +9,12 @@ import LoginScreen from "../screens/LoginScreen";
 import SignUpScreen from "../screens/SignUpScreen";
 import DirectoryScreen from "../screens/public/DirectoryScreen";
 import MemberHomeScreen from "../screens/MemberHomeScreen";
+import PersonalTrainingScreen from "../screens/public/PersonalTrainingScreen";
+import WellnessScreen from "../screens/public/WellnessScreen";
+import AboutUsScreen from "../screens/public/AboutUsScreen";
+import PricingPlanScreen from "../screens/public/PricingPlanScreen";
+import ContactUsScreen from "../screens/public/ContactUsScreen";
+import ThankYouScreen from "../screens/public/ThankYouScreen";
 
 // Member screens (example)
 // import MemberHomeScreen from "../screens/members/MemberHomeScreen";
@@ -23,12 +29,110 @@ function LoadingScreen() {
   );
 }
 
+// function PublicStack() {
+//   return (
+//     <Stack.Navigator screenOptions={{ headerShown: false }}>
+//       <Stack.Screen name="Login_Screen" component={LoginScreen} />
+//       <Stack.Screen name="Sign_Up_Screen" component={SignUpScreen} />
+//       <Stack.Screen name="Directory_Screen" component={DirectoryScreen} />
+//     </Stack.Navigator>
+//   );
+// }
 function PublicStack() {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Login_Screen" component={LoginScreen} />
-      <Stack.Screen name="Sign_Up_Screen" component={SignUpScreen} />
-      <Stack.Screen name="Directory_Screen" component={DirectoryScreen} />
+      <Stack.Screen
+        name="Login_Screen"
+        component={LoginScreen}
+        options={{
+          headerShown: true,
+          headerTransparent: true,
+          headerTitle: "",
+          // headerBackTitleVisible: false,
+        }}
+      />
+      <Stack.Screen
+        name="Sign_Up_Screen"
+        component={SignUpScreen}
+        options={{
+          headerShown: true,
+          headerTransparent: true,
+          headerTitle: "",
+          // headerBackTitleVisible: false,
+        }}
+      />
+      <Stack.Screen
+        name="Directory_Screen"
+        component={DirectoryScreen}
+        options={{
+          headerShown: true,
+          headerTransparent: true,
+          headerTitle: "",
+          // headerBackTitleVisible: false,
+        }}
+      />
+
+      {/* Detail screens WITH headers */}
+      <Stack.Screen
+        name="Personal_Training_Screen"
+        component={PersonalTrainingScreen}
+        options={{
+          headerShown: true,
+          headerTransparent: true,
+          headerTitle: "",
+          // headerBackTitleVisible: false,
+        }}
+      />
+      <Stack.Screen
+        name="Wellness_Screen"
+        component={WellnessScreen}
+        options={{
+          headerShown: true,
+          headerTransparent: true,
+          headerTitle: "",
+          // headerBackTitleVisible: false,
+        }}
+      />
+      <Stack.Screen
+        name="About_Us_Screen"
+        component={AboutUsScreen}
+        options={{
+          headerShown: true,
+          headerTransparent: true,
+          headerTitle: "",
+          // headerBackTitleVisible: false,
+        }}
+      />
+      <Stack.Screen
+        name="Pricing_Plan_Screen"
+        component={PricingPlanScreen}
+        options={{
+          headerShown: true,
+          headerTransparent: true,
+          headerTitle: "",
+          // headerBackTitleVisible: false,
+        }}
+      />
+      <Stack.Screen
+        name="Contact_Us_Screen"
+        component={ContactUsScreen}
+        options={{
+          headerShown: true,
+          headerTransparent: true,
+          headerTitle: "",
+          // headerBackTitleVisible: false,
+        }}
+      />
+      <Stack.Screen
+        name="Thank_You_Screen"
+        component={ThankYouScreen}
+        options={{
+          headerShown: false,
+          headerTransparent: true,
+          headerTitle: "",
+          // headerBackTitleVisible: false,
+        }}
+      />
     </Stack.Navigator>
   );
 }
@@ -36,7 +140,16 @@ function PublicStack() {
 function MemberStack() {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Member_Home" component={MemberHomeScreen} />
+      <Stack.Screen
+        name="Member_Home"
+        component={MemberHomeScreen}
+        options={{
+          headerShown: true,
+          headerTransparent: true,
+          headerTitle: "",
+          // headerBackTitleVisible: false,
+        }}
+      />
     </Stack.Navigator>
   );
 }
---
+++ src/screens/member/MemberHomeScreenMerge.js
@@ -0,0 +1,136 @@
+import { ScrollView, View, Text, TouchableOpacity, Image } from "react-native";
+
+import DumbellIcon from "../../assets/icons/dumbell.svg";
+import ClipboardIcon from "../../assets/icons/clipboard.svg";
+import ArrowIcon from "../../assets/icons/left-round.svg";
+
+const VerticalLine = () => (
+  <View
+    style={{
+      height: "75%",
+      alignSelf: "center",
+      width: 1,
+      backgroundColor: "#FFFFFF",
+    }}
+  ></View>
+);
+
+const HomeScreen = () => {
+  return (
+    <ScrollView style={{ flex: 1, backgroundColor: "#131313" }}>
+      <View
+        style={{
+          flexDirection: "row",
+          paddingVertical: 8,
+          paddingHorizontal: 8,
+          justifyContent: "space-evenly",
+        }}
+      >
+        <TouchableOpacity
+          style={{ justifyContent: "center", alignItems: "center" }}
+        >
+          <DumbellIcon width={42} height={42} />
+          <Text style={{ color: "#FFFFFF", fontSize: 12 }}>Workout</Text>
+        </TouchableOpacity>
+        <VerticalLine />
+        <TouchableOpacity
+          style={{ justifyContent: "center", alignItems: "center" }}
+        >
+          <ClipboardIcon width={42} height={42} />
+          <Text style={{ color: "#FFFFFF", fontSize: 12 }}>Progress</Text>
+        </TouchableOpacity>
+        <VerticalLine />
+        <TouchableOpacity>
+          <Text style={{ color: "#FFFFFF", fontSize: 12 }}>Nutrition</Text>
+        </TouchableOpacity>
+        <VerticalLine />
+        <TouchableOpacity>
+          <Text style={{ color: "#FFFFFF", fontSize: 12 }}>Community</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={{ padding: 16 }}>
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <Text style={{ color: "#FFFFFF", fontSize: 18 }}>
+            Recommendations
+          </Text>
+          <TouchableOpacity
+            style={{ flexDirection: "row", alignItems: "center" }}
+          >
+            <Text style={{ color: "#FFFFFF", marginRight: 4 }}>See All</Text>
+            <ArrowIcon
+              width={16}
+              height={16}
+              fill="#c62828"
+              style={{ transform: [{ rotate: "180deg" }] }}
+            />
+          </TouchableOpacity>
+        </View>
+        <View style={{ flexDirection: "row", paddingVertical: 16 }}>
+          <TouchableOpacity
+            style={{
+              borderRadius: 16,
+              backgroundColor: "#FFF",
+              height: 150,
+              width: "49%",
+              marginRight: "2%",
+            }}
+          >
+            <Image />
+            <View>
+              <Text>hello</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{ borderRadius: 16, backgroundColor: "#FFF", width: "49%" }}
+          >
+            <Image />
+            <View>
+              <Text>hello</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </View>
+      <View
+        style={{
+          backgroundColor: "#c62828",
+          height: 200,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <TouchableOpacity
+          style={{
+            flexDirection: "row",
+            backgroundColor: "#202020",
+            borderRadius: 20,
+            width: "85%",
+            height: "60%",
+          }}
+        >
+          <View
+            style={{
+              justifyContent: "center",
+              alignItems: "center",
+              padding: 16,
+              width: "50%",
+            }}
+          >
+            <Text style={{ color: "#FFF", fontSize: 24, textAlign: "center" }}>
+              Weekly Challenge
+            </Text>
+            <Text style={{ color: "#FFF" }}>Plank With Hip Twist</Text>
+          </View>
+          <Image />
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
+  );
+};
+
+export default HomeScreen;
