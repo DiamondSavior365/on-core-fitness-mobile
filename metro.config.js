d714b78113001ff77b2111214a9b6bf3f3a79e65
Diamond Savior <devbydiamond24.7@gmail.com>
2026-01-05 17:13:33 -0800

    Configure Metro to support SVG icons with react-native-svg

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,30 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require('expo/metro-config');
+const { getDefaultConfig } = require("expo/metro-config");
 
-/** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname);
+module.exports = (() => {
+  const config = getDefaultConfig(__dirname);
 
-module.exports = config;
+  // Enable SVG transformer
+  config.transformer.babelTransformerPath = require.resolve(
+    "react-native-svg-transformer"
+  );
+
+  // Tell Metro not to treat SVGs as assets
+  config.resolver.assetExts = config.resolver.assetExts.filter(
+    (ext) => ext !== "svg"
+  );
+
+  // Tell Metro to treat SVGs as source files
+  config.resolver.sourceExts.push("svg");
+
+  return config;
+})();
+
+// ------------------------ OLD CONFIG FILE ---------------------------
+
+// // Learn more https://docs.expo.io/guides/customizing-metro
+// const { getDefaultConfig } = require('expo/metro-config');
+
+// /** @type {import('expo/metro-config').MetroConfig} */
+// const config = getDefaultConfig(__dirname);
+
+// module.exports = config;
