6946c1d9db01a53ebbb07d40627e2603b9a5ab7a
Diamond Savior <devbydiamond24.7@gmail.com>
2026-01-12 07:22:59 -0800

    Style MemberHomeScreen.js

---
+++ src/screens/member/MemberHomeScreenTemp.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+// import { useAuth } from "../../auth/AuthContext";
+import { useAuth } from "../../auth/AuthContext";
+
+export default function MemberHomeScreen() {
+  const { user, signOut } = useAuth();
+
+  return (
+    <View style={styles.text}>
+      <Text>Welcome: {user?.email}</Text>
+
+      <TouchableOpacity
+        onPress={signOut}
+        style={{ marginTop: 16, padding: 14 }}
+      >
+        <Text>Logout</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  text: {
+    marginTop: 80,
+    padding: 20,
+  },
+});
