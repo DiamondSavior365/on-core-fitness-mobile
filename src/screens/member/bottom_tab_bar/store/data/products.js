530115107db7c4a661e0bf88ebd881cc59658591
Diamond Savior <devbydiamond24.7@gmail.com>
2026-05-19 05:41:05 -0700

    Create Data File products.js

---
+++ src/screens/member/bottom_tab_bar/store/data/products.js
@@ -0,0 +1,15 @@
+export const products = [
+  {
+    id: "mre-lite-protein",
+    name: "MRE Lite Protein Powder",
+    price: "$39.99",
+    oldPrice: "$56.99",
+    image: require("../../../../../../assets/On_Core_Fitness_Store_Images/MRE_Lite_Protien_Powder.png"),
+    isOnSale: true,
+    isFeatured: true,
+    isPopular: false,
+    discountLabel: "30% OFF",
+    category: "Supplements",
+    description: "High-protein supplement for recovery and muscle support.",
+  },
+];
